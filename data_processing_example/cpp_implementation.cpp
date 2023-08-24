8f7480a53b97bb42fefe1af210309b13914d65be
Timi <ttaiwo73@gmail.com>
2023-08-24 17:27:59 +0100

    Code Cleanup and extra tests

--- data_processing_example/cpp_implementation.cpp
+++ data_processing_example/cpp_implementation.cpp
@@ -1,53 +1,16 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <regex>
 
-#include <array>
+#include <string>
 #include <vector>
 #include <set>
 #include <map>
+#include <array>
+
 #include <cstdio>
+#include <cmath>
 
-
-/**
-VWAPer v0.1
-
-// Read file in with the following format:
-// [Stock],[Interval],[Volume Traded],[High],[Low]
-
-// Calculate the total volume traded per Stock
-// Calculate the total volume traded per Stock&Interval
-
-// Write the total volume traded per Stock&Interval as a percentage of the total volume traded per Stock to stdout:
-// [Stock],[Interval],[%Volume Traded]
-
-// Write the delimiter '#' to stdout
-
-// Write the maximum High and minimum Low for each Stock to stdout:
-// [Stock],[Day High],[Day Low]
-
-// example input:
-VOD.L 1 100 184 183.7
-BT.LN 1 300 449.4 448.2
-VOD.L 2 25 184.1 182.4
-BT.LN 2 900 449.8 449.5
-
-// example output:
-VOD.L,1,80
-BT.LN,1,25
-VOD.L,2,20
-BT.LN,2,75
-#
-VOD.L,184.1,182.4
-BT.LN,449.8,448.2
-
-**/
-
-
-// Add data validation checks notifying the user of empty lines
-
-#define DELIMITER "#"
+// Future feature -> Add data validation checks notifying the user of empty lines & incorrect format
 
 typedef std::string string;
 typedef std::array<size_t, 4> fourUnsignedIntArray;
@@ -71,7 +34,7 @@ bool determineVersionReq(string * const &firstArg) {
     */
 
     if (*firstArg == "version") {
-        std::cout << "VWAPer version 0.1" << std::endl;
+        std::cout << "version 1" << std::endl;
         return true;
     } else { 
         return false; 
@@ -108,18 +71,17 @@ std::vector<StockRecord> read_stocktrade_file(string fileLocation) {
     */
 
    string holdLine {""};
-   int lines {0};                   // holds the number of lines read from file
    std::vector<StockRecord> stockFrame {};
 
    std::ifstream stockInfoFile {fileLocation};    // Open file
+                                                  // Add file Error Handling
     
     if (stockInfoFile.is_open()) {
-        while (stockInfoFile)
+        while (stockInfoFile)                     // Fix the double reading of the last line in file
         {
+            
             std::getline(stockInfoFile, holdLine);    // Puts a line from the file into the holdLine string
             StockRecord stockHoldingVar {};
-
-            // std::cout << holdLine << std::endl;
 
             // Add some data validation here in the future
 
@@ -136,7 +98,6 @@ std::vector<StockRecord> read_stocktrade_file(string fileLocation) {
     };
 
     stockFrame.pop_back();      // added as the while loop above adds the last row twice
-
     return stockFrame;
 }
 
@@ -153,24 +114,23 @@ int main(int argc, char* argv[])
 
     bool versionRequested = determineVersionReq(&FirstArg);
     fileLoc = versionRequested ? argv[2] : argv[1];  // assigns 2nd arguement as flie location if version is requested else the 1st arguement is file loc
-    std::cout << "Reading file at: " << fileLoc << '\n' << std::endl;
+    // std::cout << "Reading file at: " << fileLoc << '\n' << std::endl;
 
     auto stockInfo = read_stocktrade_file(fileLoc);
+    // std::cout << "File Read" << std::endl;
 
     std::set<string> stockNames {};        // a set holds all stock names
-    // std::vector<string> stockNames {};
 
     std::map<string, double> stockVolTradedTotal {};
     std::map<string, double> stockHigh {};
     std::map<string, double> stockLow {};
 
     for (int i {0}; i < stockInfo.size(); ++i) {  
-        auto Record = stockInfo[i];
+        StockRecord Record = stockInfo[i];
 
         if (stockNames.count(Record.stock_name) == 0) {
 
             stockNames.insert(Record.stock_name);
-            // stockNames.push_back(Record.stock_name);
             stockVolTradedTotal[Record.stock_name] = Record.vol_traded;
             stockHigh[Record.stock_name] = Record.high;
             stockLow[Record.stock_name] = Record.low;
@@ -183,31 +143,23 @@ int main(int argc, char* argv[])
         }
     }
 
-    // for (auto stockRecord: )
-
-    /*
     // stdout [StockName],[Interval], [%Volume of the day during this interval]
     for (int i {0}; i < stockInfo.size(); ++i) {  
         auto Record = stockInfo[i];
 
         auto percentTotal {(Record.vol_traded)/stockVolTradedTotal[Record.stock_name] * 100};
 
-        // std::cout << Record.stock_name << ',' <<  Record.interval << ',' << Record.vol_traded << ',' << stockVolTradedTotal[Record.stock_name] << std::endl;
-        std::cout << Record.stock_name << ',' <<  Record.interval << ',' << percentTotal << std::endl;
+        std::cout << Record.stock_name << ',' <<  Record.interval << ',' << std::round(percentTotal) << std::endl;
 
-        }
+    }
 
-    /*
-    std::cout << DELIMITER << std::endl;
+    std::cout << '#' << std::endl;  // Delimiter to meet design spec
 
     // stdout [StockName],[High for the day], [Low for the day]
     // The expected order is alphabetically i.e A to Z
     for (string stockName : stockNames) {
         std::cout << stockName << ',' << stockHigh.at(stockName) << ',' << stockLow.at(stockName) << std::endl;
     }
-    */
-
-    // std::cout << std::endl << "code finished" << std::endl;
 
     return 0;
 }
