d5793a9d206c95b1e3004195eef6288ad279cf73
Timi <ttaiwo73@gmail.com>
2023-12-24 22:03:40 +0000

    add rust implementation of binary search

---
+++ binary_search_algos/binary_search_rust.rs
@@ -0,0 +1,20 @@
+fn bin_search(arr: &[i32], target: &i32){
+
+    // start_ind = 
+    // end_ind = 
+    // mid_point = 
+
+    println!("{}", arr.len());
+    
+}
+
+
+fn main() {
+
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+    let target_value : i32 = 73;
+
+    // println!("the target value is: {}", target_value);
+    bin_search(primes, target_value)
+
+}
