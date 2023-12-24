a937160959ad9b5c25f45c32bd90b00d87bff149
Timi <ttaiwo73@gmail.com>
2023-12-24 22:21:43 +0000

    implemented a bubble sort in python

---
+++ sorting_algos/bubble_sort.py
@@ -0,0 +1,31 @@
+arr:list = [3, 2, 4, 1, 5]
+
+arr_sorted = False
+runs = 1
+
+print(f"run {runs}: The arr is {arr}")
+
+def swap(arr, current, next):
+    temp = arr[current]
+    arr[current] = arr[next]
+    arr[next] = temp
+
+while not arr_sorted:
+    num_changes = 0
+    for i in range(len(arr)):
+        current = i
+        next = i+1
+
+        if next == len(arr):
+            break
+
+        if arr[current] > arr[next]:
+            swap(arr, current, next)
+            num_changes += 1
+    
+    runs += 1
+    if num_changes == 0:
+        break
+    print(f"run {runs}: The arr is {arr}")
+
+# print(arr)
