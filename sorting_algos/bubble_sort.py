2544c06207627906a17dd5fbaaf561e942782947
Timi <ttaiwo73@gmail.com>
2023-12-25 15:48:16 +0000

    create main.py to encapsulate sort tests

--- sorting_algos/bubble_sort.py
+++ sorting_algos/bubble_sort.py
@@ -1,28 +1,24 @@
-from swap import *
+from swap import swap
 
-arr : list = [3, 2, 4, 1, 5]
-
-arr_sorted = False
-runs = 1
-
-print(f"run {runs}: The arr is {arr}")
-
-while not arr_sorted:
-    num_changes = 0
-    for i in range(len(arr)):
-        current = i
-        next = i+1
-
-        if next == len(arr):
-            break
-
-        if arr[current] > arr[next]:
-            swap(arr, current, next)
-            num_changes += 1
-    
-    runs += 1
-    if num_changes == 0:
-        break
+def bubble_sort(arr: list):
+    runs: int = 1
+    print("This is a bubble sort")
     print(f"run {runs}: The arr is {arr}")
+    while True:
+        num_changes = 0
+        for i in range(len(arr)):
+            current = i
+            next = i+1
 
-# print(arr)
+            if next == len(arr):
+                break
+
+            if arr[current] > arr[next]:
+                swap(arr, current, next)
+                num_changes += 1
+        
+        runs += 1
+        if num_changes == 0:
+            break
+        print(f"run {runs}: The arr is {arr}")
+
---
+++ sorting_algos/main.py
@@ -0,0 +1,6 @@
+from bubble_sort import bubble_sort
+
+arr : list = [3, 2, 4, 1, 5, 0]
+arr2 : list = [8, 2, 4, 6, 9, 7, 10, 1, 5, 3]
+
+bubble_sort(arr2)
