22076c52ace9b5cc5c32dfe736684cf8ea46713c
Timi <ttaiwo73@gmail.com>
2023-12-25 16:14:35 +0000

    started work on mergesort

--- sorting_algos/main.py
+++ sorting_algos/main.py
@@ -3,4 +3,17 @@ from bubble_sort import bubble_sort
 arr : list = [3, 2, 4, 1, 5, 0]
 arr2 : list = [8, 2, 4, 6, 9, 7, 10, 1, 5, 3]
 
-bubble_sort(arr2)
+# bubble_sort(arr2)
+
+def merge_sort(arr: list):
+
+    midpoint = int(len(arr)/2)
+    
+    if len(arr) > 1:
+        left_arr = arr[:midpoint]
+        
+        return arr
+
+
+
+print(merge_sort(arr))
---
+++ sorting_algos/merge_sort.py
@@ -0,0 +1,8 @@
+from swap import swap
+
+def merge_sort(arr: list):
+    runs: int = 1
+    print("This is a bubble sort")
+
+
+
